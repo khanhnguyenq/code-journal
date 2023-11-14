dcbbf15c9fc95c36d5d3425449038532a31b55d1
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-14 17:50:49 +0000

    Added conditional statement to add JSON #1

--- js/data.js
+++ js/data.js
@@ -1,13 +1,19 @@
 /* exported data */
 
-const data = {
+let data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
 
+const previousDataJSON = localStorage.getItem('data-object');
+
 window.addEventListener('beforeunload', function (event) {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem('data-object', dataJSON);
 });
+
+if (localStorage.getItem('data-object') !== null) {
+  data = JSON.parse(previousDataJSON);
+}
