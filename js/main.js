6aef6da7227c3d65eb6fc35f2cb7e379cde79ebf
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-14 17:16:42 +0000

    Added submit event listener to form element #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* global data */
+
 const $photoUrl = document.querySelector('#url');
 const $image = document.querySelector('.image');
 
@@ -5,4 +7,17 @@ $photoUrl.addEventListener('input', function (event) {
   $image.setAttribute('src', event.target.value);
 });
 
-// const $save = document.querySelector('.save');
+const $entryForm = document.querySelector('.entry-form');
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const submittedData = {};
+  submittedData.title = $entryForm.title.value;
+  submittedData.url = $entryForm.url.value;
+  submittedData.notes = $entryForm.notes.value;
+  submittedData.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(submittedData);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
+});
